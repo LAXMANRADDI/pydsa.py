6bdfb51f5371de8f10c4ace56abaafba577e04f8
LAXMAN_RADDI <159695664+LAXMANRADDI@users.noreply.github.com>
2025-11-29 21:04:23 +0530

    Add documentation for arrays in Python

---
+++ arrays.py
@@ -0,0 +1,10 @@
+ Array :An array is a continuous block of memory holding elements of the same type.
+i.e
+Index:   0   1   2   3   4
+Value:  10  20  30  40  50
+In low-level languages (C/C++), arrays have fixed size.
+In Python, arrays are implemented using lists (dynamic arrays).
+
+1]Creating Arrays (Lists) in Python
+arr = [10, 20, 30, 40]
+print(arr) # [10, 20 , 30 , 40]
