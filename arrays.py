65472196dff64655ba184967f802923f39c9d8f9
LAXMAN_RADDI <159695664+LAXMANRADDI@users.noreply.github.com>
2025-11-30 15:58:05 +0530

    Update arrays.py

--- arrays.py
+++ arrays.py
@@ -174,3 +174,29 @@ linear_search([10, 20, 30, 40], 30)
 
 
 ---
+2. Binary Search (O(log n)) — Very Important
+ Works only on a SORTED array
+Divide the array into half every step.
+
+Diagram:
+
+[10 20 30 40 50 60]
+  L     M       R
+---
+Binary Search Code (Iterative)
+
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
