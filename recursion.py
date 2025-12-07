7e60cd9952da84ea018ac56747f7aff7cfb7f544
LAXMAN_RADDI <159695664+LAXMANRADDI@users.noreply.github.com>
2025-12-07 22:37:38 +0530

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -41,3 +41,51 @@ Dry-run factorial(4)
     3 * factorial(2)
         2 * factorial(1)
             1 * factorial(0)
+
+Fibonacci Recursion
+fib(n) = fib(n-1) + fib(n-2)
+
+Code:
+def fib(n):
+    if n <= 1:
+        return n
+    return fib(n-1) + fib(n-2)
+
+Time complexity: O(2ⁿ) → VERY SLOW
+
+
+--
+ Sum of digits using recursion
+
+Question:
+Input: 1234
+Output: 1+2+3+4 = 10
+
+Code:
+def sum_digits(n):
+    if n == 0:
+        return 0
+    return (n % 10) + sum_digits(n // 10)
+
+
+---
+ Reverse a String using Recursion
+
+def reverse(s):
+    if len(s) == 0:
+        return s
+    return reverse(s[1:]) + s[0]
+
+
+---
+Tower of Hanoi (Most Famous)
+
+Moves n disks from A → C using B.
+Moves required = 2ⁿ – 1
+def hanoi(n, src, aux, dest):
+    if n == 1:
+        print(f"Move disk 1 from {src} to {dest}")
+        return 
+    hanoi(n-1, src, dest, aux)
+    print(f"Move disk {n} from {src} to {dest}")
+    hanoi(n-1, aux, src, dest)
