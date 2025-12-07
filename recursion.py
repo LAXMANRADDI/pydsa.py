d118bc648b603f62eb0be8d9269e478410844093
LAXMAN_RADDI <159695664+LAXMANRADDI@users.noreply.github.com>
2025-12-07 22:33:12 +0530

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -8,3 +8,20 @@ def fun():
 amd Every recursive function must have:
 Base Case → stopping condition
 Recursive Case → function calling itself
+
+
+Example: Print numbers 1 to n
+Code:
+def print_nums(n):
+    if n == 0:
+        return
+    print_nums(n-1)
+    print(n)
+
+Dry-run for n = 3:
+print_nums(3)
+→ print_nums(2)
+    → print_nums(1)
+        → print_nums(0) stops
+        → print 1
+    → print 2
