f23834fe356640211c62f668119b22cbb9f590bc
LAXMAN_RADDI <159695664+LAXMANRADDI@users.noreply.github.com>
2025-12-07 22:32:13 +0530

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -4,3 +4,7 @@ function calling itself until a stopping condition.
 ex:
 def fun():
     fun() #This will run forever →  infinite recursion.
+
+amd Every recursive function must have:
+Base Case → stopping condition
+Recursive Case → function calling itself
