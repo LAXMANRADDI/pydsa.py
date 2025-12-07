eb98c559043c607f570f552288f9c3a8c3a38646
LAXMAN_RADDI <159695664+LAXMANRADDI@users.noreply.github.com>
2025-12-07 22:30:34 +0530

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -1,1 +1,6 @@
 Recursion is used in linked lists, trees, backtracking, DP, quicksort, mergesort, etc.
+
+function calling itself until a stopping condition.
+ex:
+def fun():
+    fun() #This will run forever →  infinite recursion.
